5755cc176060b76cb9d842fa55c9ea7972391ac0
Hossein Hassani <hhassa52@uwo.ca>
2024-10-10 13:06:24 -0400

    Update BETDQNet.py

--- OpenAI Gym/BETDQNet.py
+++ OpenAI Gym/BETDQNet.py
@@ -12,3 +12,15 @@ from torchvision import transforms
 from prioritized_memory import Memory
 
 
+""" Training Parameters """
+EPISODES      = 250
+DISCOUNT      = 0.99
+LEARNING_RATE = 0.001
+MEMORY_SIZE   = 10_000
+EPSILON_START = 1.0
+EPSILON_END   = 0.1
+EPSILON_DECAY_PERIOD = 5_000
+BATCH_SIZE    = 64
+TRAIN_START   = 1_000
+W1 = 0.2
+W2 = 0.8
